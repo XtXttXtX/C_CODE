fa07f7bcac415bfeeec5ee4f7e79d9e9dd1ae5d6
XtXttXtX <1545901527@qq.com>
2024-10-09 16:53:36 +0800

    纠错以及调试案例

--- class_j7/class_j7/test.c
+++ class_j7/class_j7/test.c
@@ -43,19 +43,65 @@
 //	return 0;
 //}
 
-struct Stu
+//struct Stu
+//{
+//	char name[20];
+//	int age;
+//}; 
+//void print(struct Stu* ps)
+//{
+//	printf("name=%s age=%d\n", (*ps).name,(*ps).age);
+//	printf("name=%s age=%d\n", ps->name, ps->age);
+//}
+//int main()
+//{
+//	struct Stu s = { "zhangsan", 20 };
+//	print(&s);//结构体地址传参
+//	return 0;
+//}
+
+//struct S
+//{
+//	int data[1000];
+//	int num;
+//};
+//struct S s = { { 1, 2, 3, 4 }, 1000 };
+////结构体传参
+//void print1(struct S s)//形参是实参的临时拷贝
+//{
+//	printf("%d\n", s.num);
+//}
+////结构体地址传参
+//void print2(struct S* ps)//传递的是指针的大小
+//{
+//	printf("%d\n", ps->num);
+//}
+//int main()
+//{
+//	print1(s);//传结构体
+//	print2(&s);//传地址
+//	return 0;
+//}
+
+
+void test3()
 {
-	char name[20];
-	int age;
-}; 
-void print(struct Stu* ps)
+	printf("hhhhhh");
+}
+void test2()
 {
-	printf("name=%s age=%d\n", (*ps).name,(*ps).age);
-	printf("name=%s age=%d\n", ps->name, ps->age);
+	test3();
+}
+void test1()
+{
+	test2();
+}
+void test()
+{
+	test1();
 }
 int main()
 {
-	struct Stu s = { "zhangsan", 20 };
-	print(&s);//结构体地址传参
+	test();
 	return 0;
 }
